f0442f469f6de43423581eaafc40a0f581efc513
marlon <marlon.monterrosa.m@gmail.com>
2025-06-05 18:14:02 -0500

    agrego los componentes en pares: una etiqueta (JLabel) y su campo (JTextField).
    Esto se repite para nombre, apellido, fecha, etc.creo el botón "Guardar". añado una acción: cuando se hace clic, se ejecuta el método guardarProfesor().

--- src/main/java/edu/iudigital/InterfazProfesor.java
+++ src/main/java/edu/iudigital/InterfazProfesor.java
@@ -15,4 +15,25 @@ public class InterfazProfesor extends JFrame {
         setSize(400, 400);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new GridLayout(9, 2));
+
+        txtNombre = new JTextField(); txtApellido = new JTextField();
+        txtFechaNacimiento = new JTextField(); txtGenero = new JTextField();
+        txtEstatura = new JTextField(); txtPeso = new JTextField();
+        txtEspecialidad = new JTextField(); txtExperiencia = new JTextField();
+        btnGuardar = new JButton("Guardar");
+
+        add(new JLabel("Nombre:")); add(txtNombre);
+        add(new JLabel("Apellido:")); add(txtApellido);
+        add(new JLabel("Fecha Nacimiento (AAAA-MM-DD):")); add(txtFechaNacimiento);
+        add(new JLabel("Género:")); add(txtGenero);
+        add(new JLabel("Estatura:")); add(txtEstatura);
+        add(new JLabel("Peso:")); add(txtPeso);
+        add(new JLabel("Especialidad:")); add(txtEspecialidad);
+        add(new JLabel("Experiencia (años):")); add(txtExperiencia);
+        add(btnGuardar);
+
+        btnGuardar.addActionListener(e -> guardarProfesor());
+
+        setVisible(true);
+    }
 }
