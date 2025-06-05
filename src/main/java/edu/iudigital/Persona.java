a7901757524a8a1d15cf43fbbf134d105a518a23
marlon <marlon.monterrosa.m@gmail.com>
2025-06-05 17:59:30 -0500

    creo el construtor

--- src/main/java/edu/iudigital/Persona.java
+++ src/main/java/edu/iudigital/Persona.java
@@ -7,4 +7,13 @@ public class Persona {
     private String genero;
     private double estatura;
     private double peso;
+
+    public Persona(String nombre, String apellido, LocalDate fechaNacimiento, String genero, double estatura, double peso) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+        this.genero = genero;
+        this.estatura = estatura;
+        this.peso = peso;
+    }
 }
