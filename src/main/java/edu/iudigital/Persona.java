16f8b6fbedc427326a282cee85cca82bdab969d9
marlon <marlon.monterrosa.m@gmail.com>
2025-06-05 17:58:15 -0500

    creo los atributos privados de mi clas persona

--- src/main/java/edu/iudigital/Persona.java
+++ src/main/java/edu/iudigital/Persona.java
@@ -1,4 +1,10 @@
 package edu.iudigital;
 import java.time.LocalDate;
 public class Persona {
+    private String nombre;
+    private String apellido;
+    private LocalDate fechaNacimiento;
+    private String genero;
+    private double estatura;
+    private double peso;
 }
