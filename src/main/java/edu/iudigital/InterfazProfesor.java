894cba408888e4bf88f61e3baabf0fbc133101f4
marlon <marlon.monterrosa.m@gmail.com>
2025-06-05 18:10:34 -0500

    Se usa un GridLayout para organizar los componentes en forma de tabla: etiquetas + campos.

--- src/main/java/edu/iudigital/InterfazProfesor.java
+++ src/main/java/edu/iudigital/InterfazProfesor.java
@@ -9,4 +9,10 @@ import java.time.LocalDate;
 public class InterfazProfesor extends JFrame {
     private JTextField txtNombre, txtApellido, txtFechaNacimiento, txtGenero, txtEstatura, txtPeso, txtEspecialidad, txtExperiencia;
     private JButton btnGuardar;
+
+    public InterfazProfesor() {
+        setTitle("Registro de Profesores");
+        setSize(400, 400);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new GridLayout(9, 2));
 }
