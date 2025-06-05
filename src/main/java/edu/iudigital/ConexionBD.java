060cbb4687339ed4852b492c2fafb25cdeb29a06
marlon <marlon.monterrosa.m@gmail.com>
2025-06-05 17:56:03 -0500

    creo la conexion con my base de datos

--- src/main/java/edu/iudigital/ConexionBD.java
+++ src/main/java/edu/iudigital/ConexionBD.java
@@ -4,4 +4,17 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionBD {
+    public static Connection getConnection() {
+        String url = "jdbc:mysql://localhost:3306/escuela";
+        String usuario = "root";
+        String contraseña = "root";
+        try {
+            Connection conn = DriverManager.getConnection(url, usuario, contraseña);
+            System.out.println("¡Conexión exitosa a la base de datos!");
+            return conn;
+        } catch (SQLException e) {
+            System.err.println("Error al conectar a la base de datos: " + e.getMessage());
+            return null;
+        }
+    }
 }
