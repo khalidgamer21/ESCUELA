43fdbfa692f3c23b19d6f30cdd3eb8a79ffafe2c
marlon <marlon.monterrosa.m@gmail.com>
2025-06-05 18:04:25 -0500

    heredo todo super y agrego mis otros dos atributos

--- src/main/java/edu/iudigital/Profesor.java
+++ src/main/java/edu/iudigital/Profesor.java
@@ -4,5 +4,13 @@ public class Profesor extends Persona {
         private String especialidad;
         private int experiencia;
 
+        public Profesor(String nombre, String apellido, LocalDate fechaNacimiento, String genero, double estatura, double peso, String especialidad, int experiencia) {
+                super(nombre, apellido, fechaNacimiento, genero, estatura, peso);
+                this.especialidad = especialidad;
+                this.experiencia = experiencia;
+        }
+
+
+
 
 }
